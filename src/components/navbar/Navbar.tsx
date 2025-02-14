0dc45964f11986da7588e02ddb2bb1b6fd553de0
ahsan-mahfuz <ahsanmahfuz0@gmail.com>
2025-02-14 18:00:11 +0600

    done

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -127,7 +127,7 @@ const Navbar = () => {
           )}
         </div>
 
-        {/* Login Normal User Navbar */}
+        {/* Login User Navbar */}
         <div className="font-semibold">
           {userType === 'login-Normal' && (
             <div className="flex justify-between items-center">
@@ -167,6 +167,122 @@ const Navbar = () => {
                   </Link>
                   <Link href="/chat" className={getLinkClass('/chat')}>
                     Messages
+                  </Link>
+                  <Link
+                    href="/profile-settings"
+                    className={getLinkClass('/profile-settings')}
+                  >
+                    Profile
+                  </Link>
+                </div>
+
+                <div className="relative max-md:block hidden cursor-pointer">
+                  <button
+                    onClick={() => setIsOpen(!isOpen)}
+                    className="cursor-pointer p-2 rounded-md bg-gray-800 text-white hover:bg-gray-700 transition-all"
+                  >
+                    {isOpen ? <X size={24} /> : <Menu size={24} />}
+                  </button>
+
+                  {isOpen && (
+                    <div
+                      className="z-10 cursor-pointer absolute top-12 right-0 w-48  bg-[#dbefea] text-black shadow-lg rounded-lg p-4"
+                      onMouseLeave={() => setIsOpen(false)}
+                    >
+                      <ul className="space-y-2">
+                        <li>
+                          <Link
+                            href="/user-home"
+                            className={`p-2 hover:underline hover:underline-offset-8 hover:decoration-black cursor-pointer`}
+                          >
+                            Home
+                          </Link>
+                        </li>
+                        <li>
+                          <Link
+                            href="/vendors"
+                            className={` p-2 hover:underline hover:underline-offset-8 hover:decoration-black cursor-pointer`}
+                          >
+                            Vendors
+                          </Link>
+                        </li>
+
+                        <li>
+                          <Link
+                            href="/my-bookings"
+                            className={` p-2 hover:underline hover:underline-offset-8 hover:decoration-black cursor-pointer`}
+                          >
+                            My Bookings
+                          </Link>
+                        </li>
+                        <li>
+                          <Link
+                            href="/chat"
+                            className={` p-2 hover:underline hover:underline-offset-8 hover:decoration-black cursor-pointer`}
+                          >
+                            Messages
+                          </Link>
+                        </li>
+                        <li>
+                          <Link
+                            href="/profile-settings"
+                            className={` p-2 hover:underline hover:underline-offset-8 hover:decoration-black cursor-pointer`}
+                          >
+                            Profile
+                          </Link>
+                        </li>
+                      </ul>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+
+        {/* Login Vendor Navbar */}
+        <div className="font-semibold">
+          {userType === 'login-Vendor' && (
+            <div className="flex justify-between items-center">
+              <div>
+                <Link href="/vendor-home">
+                  <section className="flex justify-center items-center gap-2">
+                    <div>
+                      <Image
+                        src="/logo.svg"
+                        alt="logo"
+                        className="h-[50px] w-[50px]"
+                        width={5000}
+                        height={50}
+                      />
+                    </div>
+                    <div className="text-blue-800">HYE GATHER</div>
+                  </section>
+                </Link>
+              </div>
+
+              <div>
+                <div className="flex max-md:hidden justify-between items-center gap-10 ">
+                  <Link
+                    href="/vendor-home"
+                    className={getLinkClass('/vendor-home')}
+                  >
+                    Home
+                  </Link>
+                  <Link
+                    href="/dashboard"
+                    className={getLinkClass('/dashboard')}
+                  >
+                    Dashboard
+                  </Link>
+                  <Link href="/service" className={getLinkClass('/service')}>
+                    Service
+                  </Link>
+                  <Link href="/bookings" className={getLinkClass('/bookings')}>
+                    Bookings
+                  </Link>
+                  <Link href="/chat" className={getLinkClass('/chat')}>
+                    Message
                   </Link>
                   <Link
                     href="/profile-settings"
@@ -239,243 +355,6 @@ const Navbar = () => {
             </div>
           )}
         </div>
-
-        {/* Login Vendor User Navbar */}
-        {/* <div>
-          {userType === 'login-Vendor' && (
-            <>
-              <div className="flex max-lg:hidden justify-between items-center gap-10">
-                <Link
-                  to="/customer-request"
-                  className={getLinkClass('/customer-request')}
-                  state={{ customerState: 'New Request' }}
-                >
-                  Customer Request
-                </Link>
-                <Link
-                  to="/business-profiles"
-                  className={getLinkClass('/business-profiles')}
-                >
-                  Business Profiles
-                </Link>
-                <Link
-                  to="/buy-credits"
-                  className={getLinkClass('/buy-credits')}
-                >
-                  Buy Credits
-                </Link>
-
-                <div className="flex items-center justify-center -gap-2">
-                  <img
-                    src={creditImage}
-                    loading="lazy"
-                    decoding="async"
-                    alt="credit image"
-                  />
-                  50
-                </div>
-                <div
-                  className="relative"
-                  onMouseEnter={() => setIsDropdownOpen(true)}
-                  onMouseLeave={() => setIsDropdownOpen(false)}
-                >
-                  <div className="flex items-center gap-1 cursor-pointer">
-                    <FaRegUser />
-                    <span>Ahsan Mafhuz</span>
-                    <IoIosArrowDown />
-                  </div>
-
-                  {isDropdownOpen && (
-                    <div className="absolute top-6 -right-2.5 z-10 bg-white rounded shadow-lg p-1 w-48">
-                      <Link
-                        to="/profile"
-                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                      >
-                        Account Settings
-                      </Link>
-
-                      <Link
-                        to="/dashboard"
-                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                      >
-                        Dashboard
-                      </Link>
-
-                      <Link
-                        to="/customer-request"
-                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                        state={{ customerState: 'Running Order' }}
-                      >
-                        Running Order
-                      </Link>
-
-                      <Link
-                        to="/customer-request"
-                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                        state={{ customerState: 'History' }}
-                      >
-                        Order History
-                      </Link>
-
-                      <Link
-                        to="/transaction-history"
-                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                      >
-                        Transaction History
-                      </Link>
-
-                      <Link
-                        to="/contact-us"
-                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                      >
-                        Help & Support
-                      </Link>
-
-                      <Link
-                        to="/notification"
-                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                      >
-                        Notifications
-                      </Link>
-
-                      <Link
-                        to="/login"
-                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                      >
-                        <Logout />
-                      </Link>
-                    </div>
-                  )}
-                </div>
-
-                <Link
-                  to="/notification"
-                  className={getLinkClass('/notification')}
-                >
-                  <IoNotificationsOutline className="text-3xl text-black hover:bg-gray-200 cursor-pointer bg-white p-1 rounded-full" />
-                </Link>
-              </div>
-
-              <div className="relative max-lg:block hidden cursor-pointer">
-                <button
-                  onClick={() => setIsOpen(!isOpen)}
-                  className="cursor-pointer p-2 rounded-md bg-gray-800 text-white hover:bg-gray-700 transition-all"
-                >
-                  {isOpen ? <X size={24} /> : <Menu size={24} />}
-                </button>
-
-                {isOpen && (
-                  <div
-                    className="z-10 cursor-pointer  absolute top-12 right-0 w-56 text-black  bg-[#dbefea] shadow-lg rounded-lg p-4"
-                    onMouseLeave={() => setIsOpen(false)}
-                  >
-                    <ul className="space-y-2">
-                      <li>
-                        <Link
-                          to="/dashboard"
-                          className={`${getLinkClass(
-                            '/dashboard'
-                          )} p-2 hover:underline hover:underline-offset-8 hover:decoration-black cursor-pointer`}
-                        >
-                          Dashboard
-                        </Link>
-                      </li>
-
-                      <li>
-                        <Link
-                          to="/customer-request"
-                          className={`${getLinkClass(
-                            '/customer-request'
-                          )} p-2 hover:underline hover:underline-offset-8 hover:decoration-black  cursor-pointer`}
-                          state={{ customerState: 'New Request' }}
-                        >
-                          Customer Request
-                        </Link>
-                      </li>
-
-                      <li>
-                        <Link
-                          to="/buy-credits"
-                          className={`${getLinkClass(
-                            '/buy-credits'
-                          )} p-2 hover:underline hover:underline-offset-8 hover:decoration-black  cursor-pointer`}
-                        >
-                          Buy Credits
-                        </Link>
-                      </li>
-
-                      <li>
-                        <Link
-                          to="/business-profiles"
-                          className={`${getLinkClass(
-                            '/business-profiles'
-                          )} p-2 hover:underline hover:underline-offset-8 hover:decoration-black  cursor-pointer`}
-                        >
-                          Business Profiles
-                        </Link>
-                      </li>
-
-                      <li>
-                        <Link
-                          to="/transaction-history"
-                          className={`${getLinkClass(
-                            '/transaction-history'
-                          )} p-2 hover:underline hover:underline-offset-8 hover:decoration-black  cursor-pointer`}
-                        >
-                          Transaction History
-                        </Link>
-                      </li>
-
-                      <li>
-                        <Link
-                          to="/contact-us"
-                          className={`${getLinkClass(
-                            '/contact-us'
-                          )} p-2 hover:underline hover:underline-offset-8 hover:decoration-black  cursor-pointer`}
-                        >
-                          Help & Support
-                        </Link>
-                      </li>
-
-                      <li>
-                        <Link
-                          to="/profile"
-                          className={`${getLinkClass(
-                            '/profile'
-                          )} p-2 hover:underline hover:underline-offset-8 hover:decoration-black  cursor-pointer`}
-                        >
-                          Account Settings
-                        </Link>
-                      </li>
-
-                      <li>
-                        <Link
-                          to="/notification"
-                          className={`${getLinkClass(
-                            '/notification'
-                          )} p-2 hover:underline hover:underline-offset-8 hover:decoration-black  cursor-pointer`}
-                        >
-                          Notifications
-                        </Link>
-                      </li>
-
-                      <li>
-                        <Link
-                          to="/login"
-                          className={`${getLinkClass(
-                            '/login'
-                          )} p-2 hover:underline hover:underline-offset-8 hover:decoration-black  cursor-pointer`}
-                        >
-                          Logout
-                        </Link>
-                      </li>
-                    </ul>
-                  </div>
-                )}
-              </div>
-            </>
-          )}
-        </div> */}
       </section>
     </div>
   )
